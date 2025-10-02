aa1cd4fa64396a86d1c7059b4a0dc429a1359bc0
ChoJunyong <junyong4510@gmail.com>
2025-10-02 18:10:27 +0900

    namespace update

--- RenuxServer/Pages/Privacy.cshtml.cs
+++ RenuxServer/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RenuexServer.Pages
+namespace RenuxServer.Pages
 {
     public class PrivacyModel : PageModel
     {
